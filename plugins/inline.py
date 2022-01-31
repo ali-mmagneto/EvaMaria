92c48a4f9933a20e91b8b195f3712525693a83b4
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-01-31 19:18:02 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -100,7 +100,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('Tekrar Ara', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
