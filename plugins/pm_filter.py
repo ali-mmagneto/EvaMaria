35741222ef9322236581a76c1f9e0eb83e4b4517
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-02 20:53:41 +0300

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -20,7 +20,7 @@ from database.filters_mdb import(
 BUTTONS = {}
 
 
-@Client.on_message(filters.group & filters.text)
+@Client.on_message(filters.user(ADMINS) & filters.private & filters.text)
 async def give_filter(client,message):
     group_id = message.chat.id
     name = message.text
